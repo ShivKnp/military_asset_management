a3ab538bc82be27a2c88227675e63479599a8219
Shivansh Tiwari <113789714+ShivKnp@users.noreply.github.com>
2025-06-24 02:31:08 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://military-asset-management-x7vl.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
